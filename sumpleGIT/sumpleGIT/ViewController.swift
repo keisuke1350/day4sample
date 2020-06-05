88a0b2f252a861689d0b8963c62b18090b05c596
葛西　佳祐 <keisuke1350@gmail.com>
2020-06-05 19:32:37 +0900

    first commit

---
+++ sumpleGIT/sumpleGIT/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sumpleGIT
+//
+//  Created by 葛西　佳祐 on 2020/06/05.
+//  Copyright © 2020 葛西　佳祐. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
